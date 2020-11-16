c1789aa069df1e87b9c65d56cf49704a6a214c09
Tamanna Motaleb <Tamannamotaleb@gmail.com>
2020-11-16 11:02:57 -0500

    fixed up data, and added a get request, pricehistory seems to not show properly

--- server/index.js
+++ server/index.js
@@ -1,21 +1,27 @@
 const express = require('express');
-const db = require('../database/index.js');
+const {Home, db} = require('../database/index.js');
 let app = express();
 
 const bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.status(200).send('success');
 });
 
 app.get('/api/home-description', (req, res) => {
-  console.log(req.body)
-  //multiple db calls , quieries
-  //1, quiery, .then (repeat)
-
+ db.collection('Home').find((err, results) => {
+  if (err) {
+    console.log(err)
+    res.sendStatus(404)
+  } else {
+    console.log(results)
+    res.send(JSON.stringify(results))
+  }
+ })
 })
 
 let port = 3000;
